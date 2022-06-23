050c0fb4efba624c77d34ce94ee3bc67ada2a21c
Chadevenrud <87739913+Chadevenrud@users.noreply.github.com>
2022-06-23 15:03:17 -0700

    added new products component

--- src/components/routes/Shop/shop.component.jsx
+++ src/components/routes/Shop/shop.component.jsx
@@ -1,7 +1,18 @@
-const Shop = () => (
-  <div>
-    <div>This is the shop page</div>
-  </div>
-);
+import { useContext } from "react";
+import { ProductsContext } from "../../../contexts/products.context";
+import ProductCard from "../../product-card/product-card.component";
+import "./shop.styles.scss";
+
+const Shop = () => {
+  const { products } = useContext(ProductsContext);
+  console.log(products);
+  return (
+    <div className="products-container">
+      {products.map((product) => {
+        return <ProductCard key={product.id} products={product} />;
+      })}
+    </div>
+  );
+};
 
 export default Shop;
